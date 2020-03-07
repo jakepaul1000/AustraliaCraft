11a9636545b8211762f9c10caa3ad831fe4d548b
jakepaul1000 <56169794+jakepaul1000@users.noreply.github.com>
2020-03-07 14:48:17 -0800

    Add files via upload

---
+++ AustraliaCraft/src/main/java/DariusD/AustraliaMod/AustraliaCraft.java
@@ -0,0 +1,18 @@
+package DariusD.AustraliaMod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "AustraliaCraft", name = "AustraliaCraft", version = "1.0.0")
+public class AustraliaCraft {
+	
+	public static void preInit() {
+	}
+	
+	public static void init() {
+	}
+	
+	public static void postInit() {
+	}
+	
+
+}
